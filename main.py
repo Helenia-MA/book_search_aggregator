921d7f809393be103b429858b35e5998aad6c020
Helenia <hm4945@princeton.edu>
2025-05-01 03:33:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from tabulate import tabulate
 from book_search import search_google_books, search_open_library, combine_data
+
+def display_results(results):
+    headers = ["Source", "Title", "Author", "Link"]
+    table = [[r['source'], r['title'], r['author'], r['link']] for r in results]
+    print(tabulate(table, headers=headers, tablefmt="fancy_grid"))
+    
 def main() :
     print("Welcome to the Book aggregator!")
     book = input("Enter a book title: ")
