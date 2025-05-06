ab3268b56ef44b608168b4d59d8e8ec9aef8521b
Helenia Mungu-Arere <hm4945@princeton.edu>
2025-05-06 18:35:57 -0400

    changing display

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from tabulate import tabulate
+#from prettytable import PrettyTable
 from book_search import search_google_books, search_open_library, combine_data
 
 def display_results(results):
     headers = ["Source", "Title", "Author", "Link"]
     table = [[r['source'], r['title'], r['author'], r['link']] for r in results]
     print(tabulate(table, headers=headers, tablefmt="fancy_grid"))
+    #table = PrettyTable()
+    #table.field_names = ["Source", "Title", "Author", "Link"]
+
+    #for result in results:
+    #    table.add_row([result["source"], result["title"], result["author"], result["link"]])
+
+    #print(table)
     
 def main() :
     print("Welcome to the Book aggregator!")
