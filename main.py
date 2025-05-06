4c1d6c0ecf3af452910a6320ace6b355ff06beba
Helenia Mungu-Arere <hm4945@princeton.edu>
2025-05-06 18:49:36 -0400

    remove the redundant search file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tabulate import tabulate
 #from prettytable import PrettyTable
 from book_search import search_google_books, search_open_library, combine_data
 
+# displays the results of the book search from the different sites in a tabulated format
 def display_results(results):
     headers = ["Source", "Title", "Author", "Link"]
     table = [[r['source'], r['title'], r['author'], r['link']] for r in results]
@@ -13,7 +14,9 @@ def display_results(results):
     #    table.add_row([result["source"], result["title"], result["author"], result["link"]])
 
     #print(table)
-    
+
+# allows one to type in the name of the book they want and gives the option for
+# the author's name to to narrow the search though this isn't mandatory    
 def main() :
     print("Welcome to the Book aggregator!")
     book = input("Enter a book title: ")
