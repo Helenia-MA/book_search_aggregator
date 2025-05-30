b1a0ed6fe0d120bd2c5da147cb301fb041a7ff3a
Helenia <hm4945@princeton.edu>
2025-05-30 14:49:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from tabulate import tabulate
-#from prettytable import PrettyTable
 from book_search import search_google_books, search_open_library, combine_data
 
 # displays the results of the book search from the different sites in a tabulated format
@@ -7,13 +6,6 @@ def display_results(results):
     headers = ["Source", "Title", "Author", "Link"]
     table = [[r['source'], r['title'], r['author'], r['link']] for r in results]
     print(tabulate(table, headers=headers, tablefmt="fancy_grid"))
-    #table = PrettyTable()
-    #table.field_names = ["Source", "Title", "Author", "Link"]
-
-    #for result in results:
-    #    table.add_row([result["source"], result["title"], result["author"], result["link"]])
-
-    #print(table)
 
 # allows one to type in the name of the book they want and gives the option for
 # the author's name to to narrow the search though this isn't mandatory    
