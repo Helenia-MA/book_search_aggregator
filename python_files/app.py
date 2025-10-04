ed3aefd6cd62bd85eda9446a53d03323c74407f3
Helenia <hm4945@princeton.edu>
2025-10-04 06:03:57 +0300

    Fix syntax errors and improve book search functionality

--- python_files/app.py
+++ python_files/app.py
@@ -10,4 +10,11 @@ def home():
   if request.method ="POST":
         book = request.form.get("title")
         author = request.form.get("author")
-        g_results = search_google
+        g_results = search_google_books(book, author)
+        o_results = search_open_library(book, author)
+        results = combine_data(g_results, o_results)
+    return render_template("index.html", results=results)
+
+if __name__ = "__main__":
+    app.run(debug=True)
+
